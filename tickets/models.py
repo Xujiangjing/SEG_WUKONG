252012d1a910c7ebd8c72e41c3eb65fa6ea63dcd
leiding01 <lei.ding@kcl.ac.uk>
2025-02-04 02:45:18 +0000

    fix(test): identify some error resources.

--- tickets/models.py
+++ tickets/models.py
@@ -144,7 +144,7 @@ class Ticket(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    assigned_user = models.ForeignKey(
+    assigned_to = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
         null=True,
