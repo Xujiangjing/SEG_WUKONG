6ed28d3f1262281fac89a07c84041c62b3a17965
leiding01 <lei.ding@kcl.ac.uk>
2025-03-03 03:54:32 +0000

    Implement S3 integration logic in settings and model

--- query_hub/settings.py
+++ query_hub/settings.py
@@ -185,14 +185,21 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')      
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
-AWS_STORAGE_BUCKET_NAME = 'wukongticketsystem' 
-AWS_S3_REGION_NAME = 'eu-west-2'                            
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+      
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = 'wukongticket'
+ 
+AWS_S3_REGION_NAME = 'eu-west-2'
+                            
 AWS_S3_SIGNATURE_VERSION = 's3v4'   
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
  
-AWS_QUERYSTRING_AUTH = True                          
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+
+        
