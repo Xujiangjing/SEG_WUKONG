1891663cfbc858d2a93fff022691b1c482d4a013
leiding01 <lei.ding@kcl.ac.uk>
2025-03-11 20:40:43 +0000

    modify: change file_url to file_name

--- tickets/models.py
+++ tickets/models.py
@@ -196,7 +196,7 @@ class TicketAttachment(models.Model):
     @property
     def filename(self):
 
-        return os.path.basename(self.file.name)
+        return os.path.basename(self.filename)
 
     def __str__(self):
         return f"Attachment {self.file} for Ticket {self.ticket.id}"
