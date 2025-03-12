73c96069fd92f3f7279709577eaf8512c4482867
Xujiangjing <xujiangjing00@outlook.com>
2025-03-12 17:53:13 +0000

    update models.py to prevent warning

--- tickets/models.py
+++ tickets/models.py
@@ -233,7 +233,7 @@ class AITicketProcessing(models.Model):
         return f"AI Processing for Ticket {self.ticket.id}"
 
 class MergedTicket(models.Model):
-    primary_ticket = models.ForeignKey(Ticket, related_name='primary_ticket', on_delete=models.CASCADE,unique=True)
+    primary_ticket = models.OneToOneField(Ticket, related_name='primary_ticket', on_delete=models.CASCADE,unique=True)
     suggested_merged_tickets = models.ManyToManyField(Ticket, related_name='suggested_merged_tickets')
     approved_merged_tickets = models.ManyToManyField(Ticket, related_name='approved_merged_tickets')
     
