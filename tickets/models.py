f9c14edf71e88b791a058e52a7e0770a42ee44c4
YiLu8666 <luyi4336@gmail.com>
2025-03-19 14:10:57 +0000

    Limite title's length

--- tickets/models.py
+++ tickets/models.py
@@ -145,7 +145,7 @@ class Ticket(models.Model):
     creator = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="submitted_tickets"
     )
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     status = models.CharField(
         max_length=20, choices=STATUS_CHOICES, default="in_progress"
