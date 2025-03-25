4c4fbca076b1ee33113e9e859e2574d0634aeae4
Haichong0-0 <ho5099@hotmail.com>
2025-03-25 20:49:30 +0000

    complete unseed

--- tickets/management/commands/unseed.py
+++ tickets/management/commands/unseed.py
@@ -1,12 +1,21 @@
 from django.core.management.base import BaseCommand, CommandError
-from tickets.models import User
+from tickets.models import *
 
 class Command(BaseCommand):
     """Build automation command to unseed the database."""
     
-    help = 'Seeds the database with sample data'
+    help = "Removes all non-staff users and related ticket data from the database."
 
     def handle(self, *args, **options):
         """Unseed the database."""
 
-        User.objects.filter(is_staff=False).delete()
+        TicketAttachment.objects.all().delete()
+        TicketActivity.objects.all().delete()
+        Response.objects.all().delete()
+        AITicketProcessing.objects.all().delete()
+        MergedTicket.objects.all().delete()
+        DailyTicketClosureReport.objects.all().delete()
+        Ticket.objects.all().delete()
+        User.objects.all().delete()
+
+        self.stdout.write(self.style.SUCCESS("Successfully unseeded the database."))
